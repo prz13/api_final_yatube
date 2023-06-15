91180260abc0b3d442fa6f6025a57697faeb5a71
Dmitry <36100192+prz13@users.noreply.github.com>
2023-06-15 20:34:01 +0300

    Add_PEP8.

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -42,17 +42,15 @@ class FollowSerializer(serializers.ModelSerializer):
 
         if followed_user == follower_user:
             raise serializers.ValidationError(
-                  'Вы не можете подписаться на самого себя.'
-                                             )
+                  'Вы не можете подписаться на самого себя.')
 
         existing_follow = Follow.objects.filter(
-           user=follower_user,
-           following=followed_user
-        ).exists()
+                           user=follower_user,
+                           following=followed_user
+                           ).exists()
         if existing_follow:
             raise serializers.ValidationError(
-                'Вы уже подписаны на данного пользователя.'
-                                             )
+                'Вы уже подписаны на данного пользователя.')
 
         return data
 
