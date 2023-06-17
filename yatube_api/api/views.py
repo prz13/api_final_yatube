57424c1a8773bd3af23ddfb3a769a394b736bc06
Dmitry <36100192+prz13@users.noreply.github.com>
2023-06-17 13:36:13 +0300

    Add_new_migrait.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from posts.models import Group, Post, Comment, User
 from rest_framework import viewsets, permissions, filters
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
@@ -10,12 +11,6 @@ from .serializers import (
     PostSerializer,
     FollowSerializer
 )
-
-
-class GroupViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -28,9 +23,15 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    #permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         post = self.get_post()
