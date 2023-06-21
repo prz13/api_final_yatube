97876cb483102c87fea1669fcc1fe3dde27a9b70
Dmitry <36100192+prz13@users.noreply.github.com>
2023-06-21 15:48:50 +0300

    Fixet_pep8.

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from . models import Post
 
 
-@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author')
     search_fields = ('text',)
